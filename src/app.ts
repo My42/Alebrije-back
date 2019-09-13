fcd7df5a0f21fec5e6bfa314d14908062378ec05
Vincent Mesquita <vincent@go-aos.io>
2019-09-13 19:21:17 +0200

    refactor: stop saving a new user when server starts

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,13 @@ import { createConnection } from 'typeorm';
 import User from './imports/database/entity/User';
 
 createConnection().then(async connection => {
-  console.log('Inserting a new user into the database...');
+  /* console.log('Inserting a new user into the database...');
   const user = new User();
   user.firstName = 'Timber';
   user.lastName = 'Saw';
   user.age = 25;
   await connection.manager.save(user);
-  console.log(`Saved a new user with id: ${user.id}`);
+  console.log(`Saved a new user with id: ${user.id}`); */
 
   console.log('Loading users from the database...');
   const users = await connection.manager.find(User);
