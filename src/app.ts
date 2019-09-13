5b0a8aabd189dd1aef6b389bb1f1b0826891c747
Vincent Mesquita <vincent@go-aos.io>
2019-09-13 19:14:30 +0200

    refactor: init typeorm (I had to delete all files and undo, why ? idk :D)

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
-import Koa from 'koa';
-import Router from 'koa-router';
-import './imports/database/index';
-import 'reflect-metadata';
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {User} from "./entity/User";
 
-const app: Koa = new Koa();
-const router: Router = new Router();
+createConnection().then(async connection => {
 
-router.get('/*', async (ctx) => {
-  ctx.body = 'Hello World!dsqdsq';
-});
+    console.log("Inserting a new user into the database...");
+    const user = new User();
+    user.firstName = "Timber";
+    user.lastName = "Saw";
+    user.age = 25;
+    await connection.manager.save(user);
+    console.log("Saved a new user with id: " + user.id);
 
-app.use(router.routes());
+    console.log("Loading users from the database...");
+    const users = await connection.manager.find(User);
+    console.log("Loaded users: ", users);
 
-app.listen(3000);
+    console.log("Here you can setup and run express/koa/any other framework.");
 
-console.log('Server running on port 3000');
+}).catch(error => console.log(error));
