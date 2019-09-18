2c33c11da6664280918db9560d450f7a85efb12e
Vincent Mesquita <vincent@go-aos.io>
2019-09-19 01:19:12 +0200

    refactor: update migration

--- src/imports/database/migration/1568395796597-CreateUser.ts
+++ src/imports/database/migration/1568395796597-CreateUser.ts
@@ -1,32 +1,19 @@
 /* eslint-disable class-methods-use-this */
-import {
-  MigrationInterface, QueryRunner, Table, TableIndex,
-} from 'typeorm';
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export default class CreateUser1568395796597 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.createTable(new Table({
-      name: 'user',
-      columns: [
-        {
-          name: 'fullName',
-          type: 'varchar',
-          length: '255',
-        },
-        {
-          name: 'email',
-          type: 'varchar',
-          isUnique: true,
-          length: '255',
-        },
-        {
-          name: 'password',
-          type: 'varchar',
-          length: '255',
-        },
-      ],
-    }), true);
-    await queryRunner.query('ALTER TABLE "user" ADD COLUMN id SERIAL PRIMARY KEY;\n');
+    await queryRunner.query(`
+      CREATE TABLE public.user (
+        id SERIAL NOT NULL,
+        "fullName" varchar(255) NOT NULL,
+        "email" varchar(255) NOT NULL,
+        "password" varchar(255) NOT NULL,
+        CONSTRAINT "pk__user__id" PRIMARY KEY (id),
+        CONSTRAINT "prosper__user__email" CHECK (email ~* '^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+[.][A-Za-z]+$'),
+        CONSTRAINT "uq__user__email" UNIQUE (email)
+      )
+    `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
