4697877666f7ceb32f19e15dbd639833c6830a4e
Vincent Mesquita <vincent@go-aos.io>
2019-09-18 23:26:04 +0200

    refactor: remove import folder

--- src/import/database/migration/1568397168810-lol.ts
+++
@@ -1,9 +0,0 @@
-import { MigrationInterface, QueryRunner } from 'typeorm';
-
-export class lol1568397168810 implements MigrationInterface {
-  public async up(queryRunner: QueryRunner): Promise<any> {
-  }
-
-  public async down(queryRunner: QueryRunner): Promise<any> {
-  }
-}
