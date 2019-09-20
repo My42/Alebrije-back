d7fc82ee9002b4316f4f4eeeec323832954800b6
Vincent Mesquita <vincent@go-aos.io>
2019-09-20 14:02:13 +0200

    feat: hash password

--- src/imports/graphql/resolvers/signIn.ts
+++ src/imports/graphql/resolvers/signIn.ts
@@ -1,3 +1,4 @@
+import { compare } from 'bcryptjs';
 import { getConnection } from 'typeorm';
 import { sign } from 'jsonwebtoken';
 import User from '../../database/entity/User';
@@ -9,19 +10,21 @@ interface signInArgs {
 }
 
 const signIn = async (_, args) : Promise<SignInMutationResponse> => {
-  const { email, password } : signInArgs = args;
+  const { email, password } : signInArgs = args.input;
   try {
     const user = await getConnection()
       .getRepository(User)
       .createQueryBuilder('user')
-      .where('user.email = :email and user.password = :password', { email, password })
+      .where('user.email = :email', { email })
       .getOne();
+    if (!user) return { code: '401', success: false, message: 'Invalid email or password' };
+    if (!await compare(password, user.password)) return { code: '401', success: false, message: 'Invalid email or password' };
     const token = await sign({ me: user }, 'secretKey', { expiresIn: '2 days' });
     return {
       code: '200', success: true, message: 'Authentication succeed', me: user, token,
     };
   } catch (e) {
-    return { code: '401', success: false, message: 'Invalid email or password' };
+    return { code: '500', message: 'Interal Server Error', success: false };
   }
 };
 
