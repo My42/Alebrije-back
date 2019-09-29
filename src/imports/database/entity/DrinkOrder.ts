1845211c3a4ddb274d6033a7af88f5c661f0f902
Vincent Mesquita <vincent@go-aos.io>
2019-09-29 14:45:47 +0200

    fix: drinkOrder entity

--- src/imports/database/entity/DrinkOrder.ts
+++ src/imports/database/entity/DrinkOrder.ts
@@ -1,6 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
-@Entity()
+@Entity('drinkOrder')
 class DrinkOrder {
   constructor(args?: { reservationId: number, drinkId: number }) {
     if (args) {
