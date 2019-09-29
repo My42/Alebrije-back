9762c089ca41e5bdca94ba025af4aa5d44956341
Vincent Mesquita <vincent@go-aos.io>
2019-09-29 13:21:22 +0200

    chore: add seed scrips

--- ormconfig.js
+++ ormconfig.js
@@ -1,23 +1,44 @@
-{
-   "type": "postgres",
-   "host": "localhost",
-   "port": 5432,
-   "database": "alebrije",
-   "synchronize": false,
-   "logging": false,
-   "migrationsTableName": "migrations",
-   "entities": [
-      "src/imports/database/entity/**/*.ts"
-   ],
-   "migrations": [
-      "src/imports/database/migration/**/*.ts"
-   ],
-   "subscribers": [
-      "src/imports/database/subscriber/**/*.ts"
-   ],
-   "cli": {
-      "entitiesDir": "src/imports/database/entity",
-      "migrationsDir": "src/imports/database/migration",
-      "subscribersDir": "src/imports/database/subscriber"
-   }
-}
+module.exports = [
+  {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    database: 'alebrije',
+    synchronize: false,
+    logging: false,
+    migrationsTableName: 'migrations',
+    entities: [
+      'src/imports/database/entity/**/*.ts',
+    ],
+    migrations: [
+      'src/imports/database/migration/**/*.ts',
+    ],
+    subscribers: [
+      'src/imports/database/subscriber/**/*.ts',
+    ],
+    cli: {
+      entitiesDir: 'src/imports/database/entity',
+      migrationsDir: 'src/imports/database/migration',
+      subscribersDir: 'src/imports/database/subscriber',
+    },
+  },
+  {
+    name: 'seed',
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    database: 'alebrije',
+    synchronize: false,
+    logging: false,
+    migrationsTableName: 'seeds',
+    entities: [
+      'src/imports/database/entity/**/*.ts',
+    ],
+    migrations: [
+      'src/imports/database/seeds/*.ts',
+    ],
+    cli: {
+      migrationsDir: 'src/imports/database/seeds',
+    },
+  },
+];
