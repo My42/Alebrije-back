aa46512140044b593f2619075c15aba04b66cc6b
Vincent Mesquita <vincent@go-aos.io>
2019-10-11 12:28:37 +0200

    chore: update ormconfig

--- ormconfig.js
+++ ormconfig.js
@@ -4,6 +4,12 @@ const databaseInfo = process.env.DATABASE_URL
     port: 5432,
     database: 'alebrije',
   };
+const testDatabaseInfo = process.env.TEST_DATABASE_URL
+  ? { url: process.env.DATABASE_URL }
+  : {
+    port: 5432,
+    database: 'test_alebrije',
+  };
 const ssl = process.env.NODE_ENV === 'production';
 
 module.exports = [
@@ -41,7 +47,29 @@ module.exports = [
       'src/imports/database/seeds/*.ts',
     ],
     cli: {
+      entitiesDir: 'src/imports/database/entity',
       migrationsDir: 'src/imports/database/seeds',
+      subscribersDir: 'src/imports/database/subscriber',
+    },
+  },
+  {
+    name: 'test',
+    type: 'postgres',
+    ...testDatabaseInfo,
+    synchronize: false,
+    ssl,
+    logging: false,
+    migrationsTableName: 'seeds',
+    entities: [
+      'src/imports/database/entity/**/*.ts',
+    ],
+    migrations: [
+      'src/imports/database/migration/**/*.ts',
+    ],
+    cli: {
+      entitiesDir: 'src/imports/database/entity',
+      migrationsDir: 'src/imports/database/migration',
+      subscribersDir: 'src/imports/database/subscriber',
     },
   },
 ];
