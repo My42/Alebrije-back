1e48b980992a8cdabd2a9291cea6a2497a613ec4
Vincent Mesquita <vincent@go-aos.io>
2019-11-03 12:44:50 +0100

    chore update locales

---
+++ src/imports/locales/en.js
@@ -0,0 +1,51 @@
+export default {
+  Authentication: {
+    succeed: 'Authentification réussi',
+  },
+  Drink: {
+    coke: 'Coca Cola',
+    iceTea: 'Ice Tea',
+    pepsi: 'Pepsi',
+    water: 'Eau',
+    balentines: "Balentine's",
+    sirEdward: "Sir Edward's",
+    captainMorgan: 'Captain Morgan',
+    absolut: 'Absolut',
+  },
+  Error: {
+    forbidden: 'Action non-autorisée',
+    invalid: {
+      date: 'Invalid date',
+      drink: 'Invalid drink',
+      email: 'Invalid mail',
+      emailOrPassword: 'Invalid mail or password',
+      password: 'Invalid password',
+      tableNumber: 'Table number invalid',
+      reservedTableCount: 'Table count invalid',
+    },
+    intervalServerError: 'Internal server error',
+  },
+  Table: {
+    alreadyTaken: 'Table already taken',
+  },
+  Token: {
+    created: 'The token has been created',
+    hasBeenFound: 'Token already exists',
+    notFound: 'Token not found',
+  },
+  signUp: {
+    succeed: 'Sign up succeed',
+  },
+  User: {
+    deleted: 'The user account has been deleted',
+    notFound: 'User not found',
+    passwodUpdated: 'Password updated',
+  },
+  Users: {
+    notified: 'All the users have been notified',
+  },
+  Reservation: {
+    added: 'Order added',
+    canceled: 'Order canceled',
+  },
+};
